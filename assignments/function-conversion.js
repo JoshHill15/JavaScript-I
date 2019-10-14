636181f10ced088c2be12d75fc6d3da0c518d347
josh Hill <Josh_hill15@me.com>
2019-10-14 17:14:15 -0400

    MVP

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,38 @@
 // };
 // myFunction();
 
+let myfunc = () => console.log("function was invoked")
+myfunc()
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherfunc = param => param
+console.log(anotherfunc);
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add1 = (param1, param2) => param1 + param2
+console.log(add1(3, 7));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+let subtract1 = (param1, param2) => param1 - param2
+console.log(subtract1(4, 6));
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const three = exampleArray.map(num => num * 3);
+console.log(three);
